af1bb1946bbe07f1e459af5e3c638164c50228e6
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-05-01 21:22:18 +0600

    Header created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="">
-      <p>Hello</p>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            Copyright Naimur Rashid Rahib
+        </div>
+    );
+};
+
+export default Footer;
